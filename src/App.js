c7c8045b21304822a9ba33b33334ac2d4a1310cd
Adrian Widłak <adrian.widlak@allventures.eu>
2023-05-19 14:04:08 +0200

    Use state in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import './App.css';
 import Card from './components/card/Card';
 
 function App() {
+  const [click, setClick] = useState(2);
+
   const city = 'Warszawa'
 
   const students = [
@@ -19,16 +22,23 @@ function App() {
     }
   ]
 
+  function onButtonClick(clickMessage) {
+    setClick(click+1)
+    console.log(clickMessage);
+  }
+
   return (
     <div className="App">
       <h1>Hello, this is our first React App</h1>
-
+      <span className='click-number'> {click} </span>
       <div className='cards'>
         {/* <Card student={students[0]} city={city}></Card>
         <Card student={students[1]} city={city}></Card> */}
 
         { students.map((student, index) => <Card key={index} student={student} city={city}></Card>)}
       </div>
+
+      <button onClick={() => onButtonClick('Button pressed!')}>CLICK</button>
     </div>
   );
 }
